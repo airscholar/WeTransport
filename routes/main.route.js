dd60d8f352d01fe4d5d27b454f6244464dc8506a
Yusuf Ganiyu <airscholar@gmail.com>
2021-07-07 19:30:06 +0100

    Updated main route

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,31 +1,38 @@
 const express = require("express");
-const { check } = require("express-validator");
 const router = express.Router();
 
-const { shipmentController } = require("../controllers/index");
+const { shipmentController, courierController, driverController, customerController } = require("../controllers/index");
 
-const { customerController } = require("../controllers/index");
+const verifyToken = require("../middlewares/verifier.middleware");
 
 //customer
-router.route("/customerList").get(customerController.loadCustomerList);
-router.route("/customerAdd").get(customerController.loadCustomerAdd).post(customerController.addCustomer);
-router.route("/customerEdit").get(customerController.loadCustomerEdit);
-router.route("/customerEdit/:customer_id").get(customerController.loadCustomerEdit).put(customerController.updateCustomer);
-router.route("/customerDelete/:customer_id").delete(shipmentController.deleteDriver);
+router.route("/customerList").get((req, res) => {
+  res.redirect("/customerList/true");
+});
+router.route("/shipmentAdd").get((req, res) => {
+  res.redirect("/customerList/false");
+});
+router.route("/customerList/:shipment_status").get(verifyToken, customerController.loadCustomerList);
+router.route("/customerAdd").get(verifyToken, customerController.loadCustomerAdd).post(verifyToken, customerController.addCustomer);
+router.route("/customerEdit").get(verifyToken, customerController.loadCustomerEdit);
+router.route("/customerEdit/:customer_id").get(verifyToken, customerController.loadCustomerEdit).put(verifyToken, customerController.updateCustomer);
+router.route("/customerDelete/:customer_id").delete(verifyToken, customerController.deleteCustomer);
 
 //courier
-router.route("/courierList").get(shipmentController.loadCourierList);
-router.route("/courierAdd").get(shipmentController.loadCourierAdd);
+router.route("/courierList").get(verifyToken, courierController.loadCourierList);
+router.route("/courierAdd").get(verifyToken, courierController.loadCourierAdd);
 
 //shipment
-router.route("/shipmentList").get(shipmentController.loadShipmentList);
-router.route("/shipmentAdd").get(shipmentController.loadShipmentAdd);
+router.route("/shipmentList").get(verifyToken, shipmentController.loadShipmentList);
+router.route("/shipmentAdd/:customer_id").get(verifyToken, shipmentController.loadShipmentAdd).post(shipmentController.addShipment);
+router.route("/shipmentEdit/:shipment_id").get(verifyToken, shipmentController.loadShipmentEdit).put(verifyToken, shipmentController.updateShipment);
+router.route("/shipmentDelete/:shipment_id").delete(shipmentController.deleteShipment);
 
 //driver
-router.route("/driverList").get(shipmentController.loadDriverList);
-router.route("/driverAdd").get(shipmentController.loadDriverAdd).post(shipmentController.addDriver);
-router.route("/driverEdit").get(shipmentController.loadDriverEdit).put(shipmentController.driverUpdate);
-router.route("/driverEdit/:driver_id").get(shipmentController.loadDriverEdit).put(shipmentController.driverUpdate);
-router.route("/driverDelete/:driver_id").delete(shipmentController.deleteDriver);
+router.route("/driverList").get(verifyToken, driverController.loadDriverList);
+router.route("/driverAdd").get(verifyToken, driverController.loadDriverAdd).post(verifyToken, driverController.addDriver);
+router.route("/driverEdit").get(verifyToken, driverController.loadDriverEdit).put(verifyToken, driverController.driverUpdate);
+router.route("/driverEdit/:driver_id").get(verifyToken, driverController.loadDriverEdit).put(verifyToken, driverController.driverUpdate);
+router.route("/driverDelete/:driver_id").delete(verifyToken, driverController.deleteDriver);
 
 module.exports = router;
