8e17dbd6a7bf474fb6c862d8bcbc5da15ec6ee7b
Yusuf Ganiyu <airscholar@gmail.com>
2021-07-07 19:17:40 +0100

    Updated tracking controller

--- controllers/dashboard/tracking.controller.js
+++ controllers/dashboard/tracking.controller.js
@@ -1,9 +1,14 @@
-const { asyncHandler } = require('../../middlewares/asyncHandler.middleware');
-
+const { asyncHandler } = require("../../middlewares/asyncHandler.middleware");
+const moment = require("moment");
 const getTracking = asyncHandler(async function (req, res) {
-  res.render('tracking', { layout: 'layouts/layout_login.hbs', header_type: 'login' });
+  res.render("tracking", { layout: "layouts/layout_login.hbs", header_type: "login", date: moment().format("LLLL"), user: req.user });
+});
+
+const getTracker = asyncHandler(async function (req, res) {
+  res.render("tracker", { layout: "layouts/layout_tracker.hbs", date: moment().format("LLLL"), user: req.user });
 });
 
 module.exports = {
   getTracking,
+  getTracker,
 };
