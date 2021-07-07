501c4b1bd546807da113fc8ced9604e86eac7732
Yusuf Ganiyu <airscholar@gmail.com>
2021-07-07 19:20:00 +0100

    Updated index route

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const { securityController, trackingController } = require('../controllers/index');
+const { securityController, trackingController } = require("../controllers/index");
+const { protectRoute, authorize } = require("../middlewares/auth.middleware");
+const verifyToken = require("../middlewares/verifier.middleware");
 
 /* GET login home page. */
-router.route('/').get(securityController.loadLogin);
+router.route("/").get(verifyToken, securityController.loadLogin).post(securityController.login);
 
 /* GET registration page. */
-router.route('/register').get(securityController.loadRegistration);
+router.route("/register").get(securityController.loadRegistration);
 
 /* GET tracking page. */
-router.route('/tracking').get(trackingController.getTracking);
+router.route("/tracking").get(trackingController.getTracking);
+router.route("/tracker").get(protectRoute, authorize("User"), trackingController.getTracker);
+router.route("/test").get((req, res) => {
+  res.json({
+    message: "testing",
+  });
+});
 
 module.exports = router;
