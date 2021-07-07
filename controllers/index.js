459a1eb3f78a6e1e0af6b7ef7a7de29b361dc7eb
Yusuf Ganiyu <airscholar@gmail.com>
2021-07-07 19:17:59 +0100

    Updated index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const securityController = require("./security/security.controller");
 const trackingController = require("./dashboard/tracking.controller");
 const dashboardController = require("./dashboard/dashboard.controller");
-const shipmentController = require("./dashboard/main.controller");
+const shipmentController = require("./dashboard/shipment.controller");
 const customerController = require("./dashboard/customer.controller");
+const courierController = require("./dashboard/courier.controller");
+const driverController = require("./dashboard/driver.controller");
 
 module.exports = {
   securityController,
@@ -10,4 +12,6 @@ module.exports = {
   dashboardController,
   shipmentController,
   customerController,
+  courierController,
+  driverController,
 };
