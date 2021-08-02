5c421dbfe97d0ed25c04ee55941c5d17c617cb51
Yusuf Ganiyu <airscholar@gmail.com>
2021-08-02 23:29:24 +0100

    Added styling and assets bundle

---
+++ public/assets/templates/head_tracking.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Track Shipment | <?php echo $core->site_name; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Courier We Transport Logistics">
+    <meta name="author" content="Yusuf Ganiyu">
+    <meta name="description" content="">
+    <!-- favicon -->
+    <link rel="icon" type="image/png" sizes="56x56" href="uploads/favicon.png">
+    <!-- Bootstrap -->
+    <link href="<?php SITEURL ?>assets/theme_deprixa/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons -->
+    <link href="<?php SITEURL ?>assets/theme_deprixa/css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Slider -->
+    <link rel="stylesheet" href="<?php SITEURL ?>assets/theme_deprixa/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="<?php SITEURL ?>assets/theme_deprixa/css/owl.theme.css" />
+    <link rel="stylesheet" href="<?php SITEURL ?>assets/theme_deprixa/css/owl.transitions.css" />
+    <!-- Main css -->
+    <link href="<?php SITEURL ?>assets/theme_deprixa/css/style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
